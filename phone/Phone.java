e26dd606187ec5e6c4e8c6ee6aa9f25e357ac9ca
Paula <trefasp@yhaoo.com>
2019-04-07 12:36:25 +0300

    Create Phone class Create Brand enum and PhoneType enum

---
+++ phone/Phone.java
@@ -0,0 +1,49 @@
+
+
+public abstract class Phone {
+    private int id;
+    private Brand brand;
+    private String model;
+    private String color;
+
+    public Phone(int id, Brand brand, String model, String color) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract PhoneType type();
+}
