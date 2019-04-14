d81b82e8648b8935303bab739def68bfd6d860c9
Paula <trefasp@yhaoo.com>
2019-04-14 13:46:11 +0300

    Added phones in stock

--- src/main/Program.java
+++ src/main/Program.java
@@ -13,7 +13,6 @@ import factory.ClassicPhoneFactory;
 import factory.FlipPhoneFactory;
 import factory.SmartPhoneFactory;
 import phone.Brand;
-import phone.ClassicPhone;
 import phone.Color;
 import phone.Phone;
 import request.Request;
@@ -28,73 +27,77 @@ public class Program {
 
     private static void openManufacturerWindow(Scanner sc) {
         int myOption;
-        Phone phone=null;
+        Phone phone = null;
         System.out.println("1.Create phone \n2.Decorate phone");
-        myOption=sc.nextInt();
+        myOption = sc.nextInt();
         switch (myOption) {
             case 1:
                 System.out.println("1.ClassicPhone \n2.FlipPhone\n3.SmartPhone\n");
-                myOption=sc.nextInt();
-                phone=selectAndCreatePhone(myOption,sc);
+                myOption = sc.nextInt();
+                phone = selectAndCreatePhone(myOption, sc);
                 break;
             case 2:
                 System.out.println("Decorate your phone with: ");
                 System.out.println("1.ClassicPhone \n2.FlipPhone\n3.SmartPhone\n");
-                myOption=sc.nextInt();
-                phone=selectAndCreatePhone(myOption,sc);
+                myOption = sc.nextInt();
+                phone = selectAndCreatePhone(myOption, sc);
                 System.out.println("1.Plus package \n 2.Premium package\n 3. All packages");
-                PhoneDecorator phoneDecorator=null;
-                int optionDecoratePhone=sc.nextInt();
-                switch (optionDecoratePhone){
+                PhoneDecorator phoneDecorator = null;
+                int optionDecoratePhone = sc.nextInt();
+                switch (optionDecoratePhone) {
                     case 1:
-                        phoneDecorator=decorateWithPlusPackage(phone);
+                        phoneDecorator = decorateWithPlusPackage(phone);
                         break;
                     case 2:
-                        phoneDecorator=decorateWithPremiumPackage(phone);
+                        phoneDecorator = decorateWithPremiumPackage(phone);
                         break;
                     case 3:
-                        phoneDecorator=decorateWithPlusPackage(phone);
-                        phoneDecorator=decorateWithPremiumPackage(phone);
+                        phoneDecorator = decorateWithPlusPackage(phone);
+                        phoneDecorator = decorateWithPremiumPackage(phone);
                         break;
                 }
                 break;
         }
 
     }
-    public static  Phone selectAndCreatePhone(int option,Scanner sc){
-        Phone phone=null;
+
+    public static Phone selectAndCreatePhone(int option, Scanner sc) {
+        Phone phone = null;
         switch (option) {
             case 1:
-                phone=readPhoneSpecification(1,sc);
+                phone = readPhoneSpecification(1, sc);
                 System.out.println(phone.toString());
                 break;
             case 2:
-                phone=readPhoneSpecification(2,sc);
+                phone = readPhoneSpecification(2, sc);
                 System.out.println(phone.toString());
                 break;
             case 3:
-                phone=readPhoneSpecification(3,sc);
+                phone = readPhoneSpecification(3, sc);
                 System.out.println(phone.toString());
                 break;
         }
         return phone;
 
     }
-    public static  PhoneDecorator decorateWithPlusPackage (Phone decoratedPhone){
+
+    public static PhoneDecorator decorateWithPlusPackage(Phone decoratedPhone) {
         System.out.println("==PLUS PACKAGE==Your benefits: + 1 RAM memory, + 3GB , + OLED display, + 7000 accessories");
-        PlusDecorator plusDecorator=new PlusDecorator(decoratedPhone);
+        PlusDecorator plusDecorator = new PlusDecorator(decoratedPhone);
         System.out.println("Assemble with plus package: ");
         System.out.println(plusDecorator.toString());
         return plusDecorator;
     }
-    public static PhoneDecorator decorateWithPremiumPackage(Phone decoratedPhone){
+
+    public static PhoneDecorator decorateWithPremiumPackage(Phone decoratedPhone) {
         System.out.println("==PREMIUM PACKAGE==Your benefits: + 2 RAM memory, + 4GB , + AMOLED display, + 8000 accessories");
-        PremiumDecorator premiumDecorator=new PremiumDecorator(decoratedPhone);
+        PremiumDecorator premiumDecorator = new PremiumDecorator(decoratedPhone);
         System.out.println("Assemble with premium package: ");
         System.out.println(premiumDecorator.toString());
         return premiumDecorator;
     }
-    public static Phone readPhoneSpecification(int optionPhoneType,Scanner sc) {
+
+    public static Phone readPhoneSpecification(int optionPhoneType, Scanner sc) {
         System.out.println("Brand: ");
         Brand brand;
         System.out.println("1." + Brand.LENOVO + "\n2." + Brand.ASUS + "\n3." + Brand.HUAWEI + "\n4." + Brand.SAMSUNG);
@@ -120,7 +123,7 @@ public class Program {
         System.out.println("Color: ");
         Color color;
         System.out.println("1." + Color.BLACK + "\n2. " + Color.GOLD + "\n3." + Color.SILVER + "\n4. " + Color.WHITE);
-        myOption=sc.nextInt();
+        myOption = sc.nextInt();
         switch (myOption) {
             case 1:
                 color = Color.BLACK;
@@ -165,21 +168,22 @@ public class Program {
 
     }
 
-    public static void main(String [] args){
-        ClassicPhoneFactory classicPhone=new ClassicPhoneFactory();
-        Phone phone = classicPhone.getPhone(2500, Brand.LENOVO,"P11", Color.BLACK,4,16,"LDC");
-        Stock.getAvailablePhones().add(phone);
-        PhoneDecorator phoneDecorator = new PhoneDecorator(phone);
-        System.out.println(phone.toString());
-        System.out.println("Initial assemble with normal package: ");
-        System.out.println(phoneDecorator.toString());
-        System.out.println("Assemble with plus package: ");
-        PlusDecorator plusDecorator=new PlusDecorator(phoneDecorator);
-        System.out.println(plusDecorator.toString());
-        PremiumDecorator premiumDecorator=new PremiumDecorator(phoneDecorator);
-        System.out.println("Assemble with premium package");
-        System.out.println(premiumDecorator.toString());
+    public static void initAvailablePhone() {
+        Stock.getAvailablePhones().add(new ClassicPhoneFactory().getPhone(2500, Brand.LENOVO, "P11", Color.BLACK, 4, 16, "LDC"));
+        Stock.getAvailablePhones().add(new ClassicPhoneFactory().getPhone(500, Brand.SAMSUNG, "S3530", Color.BLACK, 50, 8, "TFT"));
+        Stock.getAvailablePhones().add(new ClassicPhoneFactory().getPhone(400, Brand.SAMSUNG, "E2530", Color.SILVER, 40, 8, "LCD"));
+        Stock.getAvailablePhones().add(new FlipPhoneFactory().getPhone(600, Brand.LENOVO, "A588T", Color.WHITE, 512, 4, "TFT"));
+        Stock.getAvailablePhones().add(new FlipPhoneFactory().getPhone(300, Brand.HUAWEI, "U3900 ", Color.BLACK, 64, 8, "TFT"));
+        Stock.getAvailablePhones().add(new SmartPhoneFactory().getPhone(1100, Brand.HUAWEI, "P20", Color.SILVER, 4000, 64, "IPS"));
+        Stock.getAvailablePhones().add(new SmartPhoneFactory().getPhone(1200, Brand.HUAWEI, "Y9", Color.SILVER, 4000, 64, "IPS "));
+        Stock.getAvailablePhones().add(new SmartPhoneFactory().getPhone(1600, Brand.SAMSUNG, "Galaxy A8", Color.BLACK, 4000, 32, "AMOLED"));
 
+    }
+
+    public static void main(String[] args) {
+
+        initAvailablePhone();
+        Phone phone = Stock.getAvailablePhones().get(0);
         Client client = new Client("Fla", "Zic", "0526698785");
         client.sendRequest("request1", phone, RequestType.QUESTION);
         client.sendRequest("request3", phone, RequestType.REPAIR);
@@ -211,7 +215,7 @@ public class Program {
         openMainWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
     }
 
-    public static void openMainWindow(Scanner scanner, Employee questionEmployee, Employee repairEmployee, Employee buyEmployee){
+    public static void openMainWindow(Scanner scanner, Employee questionEmployee, Employee repairEmployee, Employee buyEmployee) {
         System.out.println("Select an option: ");
         System.out.println("===============");
         System.out.println("1. Manufacturer");
@@ -221,58 +225,56 @@ public class Program {
         System.out.println("===============");
         scanner = new Scanner(System.in);
 
-        if(scanner.hasNext()){
+        if (scanner.hasNext()) {
             String option = scanner.next();
 
-            switch (option){
-                case "1":{
+            switch (option) {
+                case "1": {
                     openManufacturerWindow(scanner);
                     break;
                 }
-                case "2":{
+                case "2": {
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case "3":{
+                case "3": {
                     openClientWindow(scanner);
                     break;
                 }
-                case "4":{
+                case "4": {
                     return;
                 }
             }
         }
     }
 
-    private static void openClientWindow(Scanner scanner){
+    private static void openClientWindow(Scanner scanner) {
 
     }
 
-    private static void handleRequests(Employee employee){
+    private static void handleRequests(Employee employee) {
         List<Request> requests = RequestsQueue.getRequests();
 
-        if(requests.size() > 0){
-            for(Request request : requests){
+        if (requests.size() > 0) {
+            for (Request request : requests) {
                 employee.verifyCompatibility(request);
                 break;
             }
-        }
-        else{
+        } else {
             System.out.println("There are no requests!");
         }
 
-        if(requests.size() > 0){
+        if (requests.size() > 0) {
             handleRequests(employee);
         }
     }
 
-    private static void getSubordinatesForEmployee(Employee employee){
-        if(employee.getSubordinates() != null && employee.getSubordinates().size() > 0){
-            for(Employee subordinate : employee.getSubordinates()){
+    private static void getSubordinatesForEmployee(Employee employee) {
+        if (employee.getSubordinates() != null && employee.getSubordinates().size() > 0) {
+            for (Employee subordinate : employee.getSubordinates()) {
                 System.out.println(subordinate.toString());
             }
-        }
-        else{
+        } else {
             System.out.println("This employee has no subordinates!");
         }
     }
@@ -285,29 +287,27 @@ public class Program {
         }
     }
 
-    private static void getPendingReqests(){
-        if(RequestsQueue.getRequests().size() > 0){
-            for(Request request : RequestsQueue.getRequests()){
+    private static void getPendingReqests() {
+        if (RequestsQueue.getRequests().size() > 0) {
+            for (Request request : RequestsQueue.getRequests()) {
                 System.out.println(request.toString());
             }
-        }
-        else{
+        } else {
             System.out.println("There are no requests pending!");
         }
     }
 
-    private static void getResolvedRequests(){
-        if(RequestsQueue.getResolvedRequests().size() > 0){
-            for(Request request : RequestsQueue.getResolvedRequests()){
+    private static void getResolvedRequests() {
+        if (RequestsQueue.getResolvedRequests().size() > 0) {
+            for (Request request : RequestsQueue.getResolvedRequests()) {
                 System.out.println(request.toString());
             }
-        }
-        else{
+        } else {
             System.out.println("There are no resolved requests!");
         }
     }
 
-    private static void openEmployeeWindow(Scanner scanner, Employee questionEmployee, Employee repairEmployee, Employee buyEmployee){
+    private static void openEmployeeWindow(Scanner scanner, Employee questionEmployee, Employee repairEmployee, Employee buyEmployee) {
         System.out.println("=============================================");
         System.out.println("                   EMPLOYEE                  ");
         System.out.println("=============================================");
@@ -325,47 +325,47 @@ public class Program {
 //        System.out.println("AVAILABLE: " + Stock.getAvailablePhones().toString());
 //        System.out.println("SOLD: " + Stock.getSoldPhones().toString());
 
-        if(scanner.hasNext()){
+        if (scanner.hasNext()) {
             Integer optionEmployee = scanner.nextInt();
 
-            switch (optionEmployee){
-                case 1:{
+            switch (optionEmployee) {
+                case 1: {
                     handleRequests(questionEmployee);
                     addDelay(2000);
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case 2:{
+                case 2: {
                     getPendingReqests();
                     addDelay(2000);
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case 3:{
+                case 3: {
                     getResolvedRequests();
                     addDelay(2000);
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case 4:{
+                case 4: {
                     getSubordinatesForEmployee(questionEmployee);
                     addDelay(2000);
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case 5:{
+                case 5: {
                     getSubordinatesForEmployee(repairEmployee);
                     addDelay(2000);
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case 6:{
+                case 6: {
                     getSubordinatesForEmployee(buyEmployee);
                     addDelay(2000);
                     openEmployeeWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
-                case 7:{
+                case 7: {
                     openMainWindow(scanner, questionEmployee, repairEmployee, buyEmployee);
                     break;
                 }
